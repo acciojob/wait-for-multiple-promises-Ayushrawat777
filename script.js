22cb89b17dda6df1da29c8b7064df7a958d2da84
AYUSH <106034287+Ayushrawat777@users.noreply.github.com>
2024-05-23 02:16:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
-let first_promise = new Promise((resolve, reject) => {
-	setTimeout(() => {
-		resolve("Resolved First after 1 second");
-	}, 1000);
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, 'foo');
 });
-
-let second_promise = new Promise((resolve, reject) => {
-	setTimeout(() => {
-		resolve("Resolved First after 2 seconds");
-	}, 3000);
-});
-
-let third_promise = new Promise((resolve, reject) => {
-	setTimeout(() => {
-		resolve("Resolved First after 3 seconds");
-	}, 2000);
-});
-
-try {
-	let result = Promise.all([first_promise, second_promise, third_promise]);
-	result.then((data) => console.log(data));
-} catch (error) {
-	console.log(error);
-}
