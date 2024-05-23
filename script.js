45f71aa2bd2ec5fea198491806c687cc245aeb88
AYUSH <106034287+Ayushrawat777@users.noreply.github.com>
2024-05-23 16:58:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,47 +9,49 @@ const promise3 = new Promise((resolve, reject) => {
 });
 Promise.all([promise1, promise2, promise3]).then((values) => {
   const fruitList = document.getElementById("output");
+
   //Row 1
   const row = document.createElement("tr");
-  const node1 = document.createElement("td");
-  const node11 = document.createElement("td");
-  node1.textContent = "Loading...";
-  node11.textContent = "Loading...";
-  row.append(node1, node11);
+  row.textContent = "Loading...";
   fruitList.appendChild(row);
 
   setTimeout(() => {
-	  node1.textContent = "Promise 1";
+    row.textContent = "";
+    const node1 = document.createElement("td");
+    const node11 = document.createElement("td");
+    row.append(node1, node11);
+    node1.textContent = "Promise 1";
     node11.textContent = values[0];
   }, 2000);
 
   //Row 2
   const row2 = document.createElement("tr");
-  const node2 = document.createElement("td");
-  const node22 = document.createElement("td");
-  node2.textContent = "Loading...";
-  node22.textContent = "Loading...";
-  row2.append(node2, node22);
+  row2.textContent = "Loading...";
   fruitList.appendChild(row2);
 
   setTimeout(() => {
-	  node22.textContent = "Promise 2";
+    row2.textContent = "";
+    const node2 = document.createElement("td");
+    const node22 = document.createElement("td");
+    row2.append(node2, node22);
+    node2.textContent = "Promise 2";
     node22.textContent = values[1];
   }, 1000);
 
   //Row3
   const row3 = document.createElement("tr");
-  const node3 = document.createElement("td");
-  const node33 = document.createElement("td");
-  node3.textContent = "Loading...";
-  node33.textContent = "Loading...";
-  row3.append(node3, node33);
+  row3.textContent = "Loading...";
   fruitList.appendChild(row3);
 
   setTimeout(() => {
-	  node3.textContent = "Promise 3";
+    row3.textContent = "";
+    const node3 = document.createElement("td");
+    const node33 = document.createElement("td");
+    row3.append(node3, node33);
+    node3.textContent = "Promise 3";
     node33.textContent = values[2];
   }, 3000);
+//Row 4
 
   const row4 = document.createElement("tr");
   const node4 = document.createElement("td");
@@ -59,6 +61,7 @@ Promise.all([promise1, promise2, promise3]).then((values) => {
   fruitList.appendChild(row4);
   const time = 3006;
   setTimeout(() => {
-    node44.textContent = time/1000 
+    node44.textContent = time / 1000;
   }, time);
 });
+
